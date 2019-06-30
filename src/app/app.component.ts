fb6bb7e42e2078982c6fac9c06e3dce45337d372
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-06-30 14:43:04 -0400

    added minor endpoint testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Recipe } from './recipes/recipe.model';
+import { Ingredient } from './shared/ingredient.model';
 
 @Component
-({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+    ({
+        selector: 'app-root',
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
+    })
 export class AppComponent implements OnInit {
     loadedFeature = 'recipe';
     apiValues;
+
+    constructor(private _httpService: HttpClient) { }
+
+    ngOnInit() {
+        this.testEndpoints();
+    }
 
     onNavigate( feature: string ) {
         this.loadedFeature = feature;
     }
 
-    constructor(private _httpService: HttpClient) { }
+    testEndpoints() {
+        const testIngredients = [
+            {
+                name: 'onion',
+                amount: 3
+            },
+            {
+                name: 'apple',
+                amount: 2
+            }
+        ];
 
-    ngOnInit() {
+        const testRecipe = {
+            name: 'Test Recipe',
+            description: 'A test recipe.',
+            imagePath: "",
+            ingredients: testIngredients
+        };
+
         this._httpService.get('/api/recipes').subscribe(response => {
-            console.log(response);
+            console.log("get test!", response);
+        });
+
+        this._httpService.post<Recipe>('/api/recipes', testRecipe ).subscribe(response => {
+            console.log("post test!", response);
         });
     }
 }
