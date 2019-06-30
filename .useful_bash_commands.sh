ec9bbbbfab70bcd7a1400d6749290394d3ba0cc3
MatthewMSaucedo <42758985+MatthewMSaucedo@users.noreply.github.com>
2019-06-29 20:29:51 -0400

    update to bash commands

--- .useful_bash_commands.sh
+++ .useful_bash_commands.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# navigate to this directory
-function navFoodApp() {
-  cd ~/source/repos/foodApp/foodApp/
+# Variables.
+mac_file_path=~/Documents/Github/FoodApp/
+pc_file_path=~/source/repos/foodApp/foodApp/
+
+# Navigate to this directory on pc.
+function navFoodAppPc() {
+  cd $pc_file_path
+}
+
+# Navigate to this directory on mac.
+function navFoodAppMac() {
+  cd $mac_file_path
 }
